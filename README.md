6fc9329f514120d787e6baa1a4a8d9cff167788f
docrrie <docrrie@outlook.com>
2026-08-19 14:03:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# paper-repo-template
+My paper repository for reproducing paper results or conducting original research.
---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p results/{tables,figures}
+mkdir -p analysis/{scripts,notebooks}
+mkdir -p data/{raw,processed}
+mkdir -p literature
