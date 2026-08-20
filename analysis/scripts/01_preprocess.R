7190ae521bb9d3ef80524e5548b2fcc7ca68d193
docrrie <docrrie@outlook.com>
2026-08-20 08:37:50 +0800

    Edit 01.R

--- analysis/scripts/01_preprocess.R
+++ analysis/scripts/01_preprocess.R
@@ -12,54 +12,54 @@ suppressPackageStartupMessages({
 read_GEO <- function(series) {
   eSet <- getGEO(series, destdir = 'data/raw', getGPL = FALSE)[[1]]
 
-  exp <- exprs(eSet)
+  expr <- exprs(eSet)
   pd <- pData(eSet)
 
-  exp_anno <- mapIds(
+  expr_anno <- mapIds(
     hugene10sttranscriptcluster.db,
-    keys = rownames(exp),
+    keys = rownames(expr),
     column = "SYMBOL",
     keytype = "PROBEID",
     multiVals = "first"
   )
 
-  symbol <- unname(exp_anno[rownames(exp)])
+  symbol <- unname(expr_anno[rownames(expr)])
   keep <- !is.na(symbol)
 
-  exp_g <- avereps(
-    exp[keep, , drop = FALSE],
+  expr_g <- avereps(
+    expr[keep, , drop = FALSE],
     ID = symbol[keep]
   )
 
-  return(list(exp_g, pd))
+  return(list(expr_g, pd))
 }
 
 GSE_32537 <- read_GEO("GSE32537")
-exp_32537 <- GSE_32537[[1]]
+expr_32537 <- GSE_32537[[1]]
 pd_32537 <- GSE_32537[[2]]
 
 GSE_110147 <- read_GEO("GSE110147")
-exp_110147 <- GSE_110147[[1]]
+expr_110147 <- GSE_110147[[1]]
 pd_110147 <- GSE_110147[[2]]
 
 
 # organize data
 common <- intersect(
-  rownames(exp_32537),
-  rownames(exp_110147)
+  rownames(expr_32537),
+  rownames(expr_110147)
 )
 
-exp_32537_common <- exp_32537[common, , drop = FALSE]
-exp_110147_common <- exp_110147[common, , drop = FALSE]
+expr_32537_common <- expr_32537[common, , drop = FALSE]
+expr_110147_common <- expr_110147[common, , drop = FALSE]
 
-exp_combined <- cbind(
-  exp_32537_common,
-  exp_110147_common
+expr_combined <- cbind(
+  expr_32537_common,
+  expr_110147_common
 )
 
 batch <- factor(c(
-  rep("GSE32537", ncol(exp_32537_common)),
-  rep("GSE110147", ncol(exp_110147_common))
+  rep("GSE32537", ncol(expr_32537_common)),
+  rep("GSE110147", ncol(expr_110147_common))
 ))
 
 
@@ -75,28 +75,27 @@ group <- unname(c(
   lookup[pd_110147$`disease state:ch1`]
 ))
 
-phe <- data.frame(
-  row.names = colnames(exp_combined),
+targets <- data.frame(
+  row.names = colnames(expr_combined),
   batch = batch,
   group = group
 )
 
-phe <- na.omit(phe)
+targets <- na.omit(targets)
 
-exp_combined <- exp_combined[,
-  colnames(exp_combined) %in% rownames(phe),
+expr_combined <- expr_combined[,
+  colnames(expr_combined) %in% rownames(targets),
   drop = FALSE
 ]
 
 
-# generate PCA plots & correct for batch effects 
-plot_pca <- function(exp, phe) {
-  res_pca <- PCA(t(exp), scale.unit = FALSE, graph = FALSE)
-  pct <- res_pca$eig[, 2]
+# generate PCA plots & correct for batch effects
+plot_pca <- function(expr, targets) {
+  res_pca <- PCA(t(expr), scale.unit = FALSE, graph = FALSE)
 
   p <- fviz_pca_ind(
     res_pca,
-    habillage = phe$`batch`,
+    habillage = targets$`batch`,
     addEllipses = TRUE,
     ellipse.level = 0.95,
     label = "none",
@@ -106,14 +105,19 @@ plot_pca <- function(exp, phe) {
   return(p)
 }
 
-p01 <- plot_pca(exp_combined, phe)
+p01 <- plot_pca(expr_combined, targets)
 
-exp_combat <- ComBat(
-  dat = exp_combined,
-  batch = phe$batch,
+expr_combat <- ComBat(
+  dat = expr_combined,
+  batch = targets$batch,
 )
 
-p02 <- plot_pca(exp_combat, phe)
+p02 <- plot_pca(expr_combat, targets)
+
+
+# save objects
+saveRDS(expr_combat, file = "data/processed/expr.rds")
+saveRDS(targets, file = "data/processed/targets.rds")
 
 saveRDS(p01, file = "data/processed/p01.rds")
 saveRDS(p02, file = "data/processed/p02.rds")
