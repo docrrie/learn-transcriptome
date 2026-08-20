8141b3e2243d4c180bd278d97fc9937eebecbbd3
docrrie <docrrie@outlook.com>
2026-08-20 08:13:18 +0800

    Edit 01.R

--- analysis/scripts/01_preprocess.R
+++ analysis/scripts/01_preprocess.R
@@ -1,5 +1,3 @@
-options(check.names = FALSE)
-
 suppressPackageStartupMessages({
   library(GEOquery)
   library(hugene10sttranscriptcluster.db)
@@ -10,7 +8,7 @@ suppressPackageStartupMessages({
   library(patchwork)
 })
 
-
+# get GEO data
 read_GEO <- function(series) {
   eSet <- getGEO(series, destdir = 'data/raw', getGPL = FALSE)[[1]]
 
@@ -45,6 +43,7 @@ exp_110147 <- GSE_110147[[1]]
 pd_110147 <- GSE_110147[[2]]
 
 
+# organize data
 common <- intersect(
   rownames(exp_32537),
   rownames(exp_110147)
@@ -90,49 +89,31 @@ exp_combined <- exp_combined[,
 ]
 
 
-theme_set(theme_classic(base_family = "Arial"))
-
-plot_pca <- function(exp) {
+# generate PCA plots & correct for batch effects 
+plot_pca <- function(exp, phe) {
   res_pca <- PCA(t(exp), scale.unit = FALSE, graph = FALSE)
   pct <- res_pca$eig[, 2]
 
-  fviz_pca_ind(
+  p <- fviz_pca_ind(
     res_pca,
     habillage = phe$`batch`,
     addEllipses = TRUE,
     ellipse.level = 0.95,
     label = "none",
-  ) +
-    labs(
-      title = "",
-      x = sprintf("PCA1 (%.1f%%)", pct[1]),
-      y = sprintf("PCA2 (%.1f%%)", pct[2])
-    )
+    title = ""
+  )
+
+  return(p)
 }
 
-p1 <- plot_pca(exp_combined)
+p01 <- plot_pca(exp_combined, phe)
 
 exp_combat <- ComBat(
-  dat = as.matrix(exp_combined),
+  dat = exp_combined,
   batch = phe$batch,
 )
 
-p2 <- plot_pca(exp_combat)
+p02 <- plot_pca(exp_combat, phe)
 
-combined <- (p1 + p2) +
-  plot_annotation(tag_levels = "A") &
-  theme(
-    plot.tag = element_text(size = 16, face = "bold"),
-    plot.tag.position = c(0.02, 0.98)
-  )
-
-ggsave(
-  filename = "results/figures/01_PCA.tif",
-  plot = combined,
-  width = 12,
-  height = 6,
-  units = "in",
-  dpi = 300,
-  device = "tiff",
-  compression = "lzw"
-)
+saveRDS(p01, file = "data/processed/p01.rds")
+saveRDS(p02, file = "data/processed/p02.rds")
